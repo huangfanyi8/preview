8a85d715ba357c0143022fd0c2a5acc1f9d0fc1d
huangfanyi <1345075672@qq.com>
2025-02-21 14:14:28 +0800

    c++20 comp algorithm

--- preview/ranges/predicate.hpp
+++ preview/ranges/predicate.hpp
@@ -3,7 +3,7 @@
 
 #include "../concepts/comparison.hpp"
 
-namespace preview
+namespace preview::ranges
 {
   struct identity
   {
@@ -37,6 +37,37 @@ namespace preview
     using is_transparent = std::true_type;
   };
   
+  namespace _details
+  {
+    template<class T,class U,class = void>
+    struct _less_builtin_ptr_cmp_base
+      :std::true_type
+    {};
+    
+    template<class T,class U>
+    struct _less_builtin_ptr_cmp_base<T,U,
+      std::void_t<decltype(operator<(std::declval<T&&>(), std::declval<U&&>())),
+        decltype(std::declval<T&&>().operator<(std::declval<U&&>()))>>
+      :std::false_type
+    {};
+    
+    template<class T,class U,class = void,class = void>
+    struct _less_builtin_ptr_cmp
+      :std::false_type
+    {};
+    
+    template<class T,class U>
+    struct _less_builtin_ptr_cmp<T,U
+      ,meta::_require_t<
+      convertible_to<T,const volatile void*>,
+      convertible_to<U,const volatile void*>,
+      same_as<bool,decltype(std::declval<T&&>()<std::declval<U&&>())>
+      >//require_t
+      >
+      :_less_builtin_ptr_cmp_base<T,U>
+    {};
+  }
+  
   struct less
   {
     template <class T,class U>
@@ -44,7 +75,16 @@ namespace preview
     noexcept(noexcept(std::declval<T>() < std::declval<U>()))
     -> std::enable_if_t<totally_ordered_with<T, U>, bool>
     {
-      return std::less<>{}(std::forward<T>(t), std::forward<U>(u));
+      if constexpr (_details::_less_builtin_ptr_cmp<T, U>::value)
+      {
+        auto x = reinterpret_cast<long long unsigned int>(
+          static_cast<const volatile void*>(std::forward<T>(t)));
+        auto y = reinterpret_cast<long long unsigned int>(
+          static_cast<const volatile void*>(std::forward<U>(u)));
+        return x < y;
+      }
+      else
+      return std::forward<T>(t)<std::forward<U>(u);
     }
     
     using is_transparent = std::true_type;
@@ -89,4 +129,5 @@ namespace preview
     using is_transparent = std::true_type;
   };
 }
+
 #endif //PREVIEW_PREDICATE_HPP
