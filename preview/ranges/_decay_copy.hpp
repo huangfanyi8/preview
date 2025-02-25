8d8c79610d2cad750b09c44cfe898e205ed1dae7
huangfanyi <1345075672@qq.com>
2025-02-25 17:02:31 +0800

    c++20decay_copy

--- preview/ranges/_decay_copy.hpp
+++ preview/ranges/_decay_copy.hpp
@@ -1,13 +1,13 @@
 #ifndef PREVIEW_RANGES_DECAY_COPY_HPP
 #define PREVIEW_RANGES_DECAY_COPY_HPP
 
-#include<utility>
+#include"../meta/meta.hpp"
 
 namespace preview
 {
     template <class T>
     constexpr std::decay_t<T> decay_copy(T &&t)
-    //noexcept(common::is_nothrow_convertible_v<T, std::decay_t<T>>)
+    noexcept(meta::is_nothrow_convertible_v<T, std::decay_t<T>>)
     {
         return std::forward<T>(t);
     }
