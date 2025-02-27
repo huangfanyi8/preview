9363e3c3a95cd71e28a593cfbca6a12ba1806624
huangfanyi <1345075672@qq.com>
2025-02-27 22:41:57 +0800

    ranges::empty

---
+++ preview/test/ranges/empty.cpp
@@ -0,0 +1,60 @@
+#include "../../ranges/empty.hpp"
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include "../../ranges/input_range.hpp"
+
+template<class R,class = std::enable_if_t<preview::ranges::input_range<R>>>
+void print(char id, R&& r)
+{
+    if (preview::ranges::empty(r))
+    {
+        std::cout << '\t' << id << ") Empty\n";
+        return;
+    }
+    
+    std::cout << '\t' << id << ") Elements:";
+    for (const auto& element : r)
+        std::cout << ' ' << element;
+    std::cout << '\n';
+}
+
+int main()
+{
+    {
+        auto v = std::vector<int>{1, 2, 3};
+        std::cout << "(1) ranges::empty uses std::vector::empty:\n";
+        print('a', v);
+        
+        v.clear();
+        print('b', v);
+    }
+    {
+        std::cout << "(2) ranges::empty uses ranges::size(initializer_list):\n";
+        auto il = {7, 8, 9};
+        print('a', il);
+        
+        print('b', std::initializer_list<int>{});
+    }
+    {
+        std::cout << "(2) ranges::empty on a raw array uses ranges::size:\n";
+        int array[] = {4, 5, 6}; // array has a known bound
+        print('a', array);
+    }
+    {
+        struct Scanty : private std::vector<int>
+        {
+            using std::vector<int>::begin;
+            using std::vector<int>::end;
+            using std::vector<int>::push_back;
+            // Note: both empty() and size() are hidden
+        };
+        
+        std::cout << "(3) calling ranges::empty on an object w/o empty() or size():\n";
+        Scanty y;
+        print('a', y);
+        y.push_back(42);
+        print('b', y);
+    }
+}
