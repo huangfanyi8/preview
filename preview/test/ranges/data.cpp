fd610da120c572ee2dad788485712cd75725e145
huangfanyi <1345075672@qq.com>
2025-02-27 17:13:44 +0800

    ranges::data

--- preview/test/ranges/data.cpp
+++ preview/test/ranges/data.cpp
@@ -18,5 +18,16 @@ int main()
     std::strcpy(a, preview::ranges::data(s));
     // [data(s), data(s) + size(s)] is guaranteed to be an NTBS
     
-    std::cout << a;
+    std::cout << a<<'\n';
+    
+    std::string src {"hello world!\n"};
+
+//  std::ranges::cdata(src)[0] = 'H'; // error, src.data() is treated as read-only
+    preview::ranges::data(src)[0] = 'H'; // OK, src.data() is a non-const storage
+    
+    char dst[20]; // storage for a C-style string
+    std::strcpy(dst, preview::ranges::cdata(src));
+    // [data(src), data(src) + size(src)] is guaranteed to be an NTBS
+    
+    std::cout << dst;
 }
